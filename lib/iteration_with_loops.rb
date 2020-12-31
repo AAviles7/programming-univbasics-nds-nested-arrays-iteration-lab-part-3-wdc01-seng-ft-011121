c80f5b85d0df7773b21c41e0abbb6010cf7ceb14
Abraham Aviles <aaaviles1995@gmail.com>
2020-12-31 21:54:27 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,7 +7,9 @@ def join_nested_strings(src)
   src.length.times do
     y = 0
     src[x].length.times do
-      if src[x][y] 
+      if src[x][y] is_a? String
+        ans += src[x][y]
+      end
       y += 1
     end
     x += 1
